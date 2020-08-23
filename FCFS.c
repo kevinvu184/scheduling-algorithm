39ed47c7b2ba4ac4ec61bff05dd2fe94fb905986
kevinvu184 <kevinvu184@gmail.com>
2020-08-23 12:25:21 +1000

    Read file

--- FCFS.c
+++ FCFS.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+   int a, b, c;
+   FILE *file;
 
+   if ((file = fopen("processes", "r")) == NULL)
+   {
+      printf("Error! opening file");
+      exit(1);
+   }
+
+   while (fscanf(file, "%d %d %d", &a, &b, &c) != -1)
+   {
+      printf("%d %d %d\n", a, b, c);
+   }
    return 0;
 }
