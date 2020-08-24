ee3cc0bd178364f15f0496b543fae730009da78d
kevinvu184 <kevinvu184@gmail.com>
2020-08-25 00:00:27 +1000

    Add

--- FCFS.c
+++ FCFS.c
@@ -1,103 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define NUMBER_OF_PROCESS 200
 
-void scan_from_file(FILE *in, int process_id[], int burst_time[], int arrival_time[]);
+typedef struct P
+{
+   int id;
+   int bt;
+   int at;
 
-void calc_complete_time(int complete_time[], int burst_time[]);
-void calc_turnaround_time(int turnaround_time[], int arrival_time[], int complete_time[]);
-void calc_waiting_time(int wait_time[], int burst_time[], int turnaround_time[]);
+   bool flag;
 
-void print_to_file(FILE *out, int process_id[], int burst_time[], int arrival_time[], int complete_time[], int turnaround_time[], int wait_time[]);
+   int ct;
+   int tt;
+   int wt;
+} P;
+
+void scan_from_file(FILE *in, P p[]);
+void print_to_file(FILE *out, P p[]);
+bool isDone(P p[]);
 
 int main()
 {
-   int process_id[NUMBER_OF_PROCESS] = {0}, burst_time[NUMBER_OF_PROCESS] = {0}, arrival_time[NUMBER_OF_PROCESS] = {0};
-   int complete_time[NUMBER_OF_PROCESS] = {0}, turnaround_time[NUMBER_OF_PROCESS] = {0}, waiting_time[NUMBER_OF_PROCESS] = {0};
+   P p[NUMBER_OF_PROCESS];
 
    // Open input - output file
    FILE *in;
    if ((in = fopen("processes", "r")) == NULL)
    {
-      printf("Error - Opening processes file.");
+      printf("Error - Opening p file.");
       exit(1);
    }
-
    FILE *out;
    if ((out = fopen("FCFS.out", "w")) == NULL)
    {
       printf("Error - Opening out file.");
       exit(1);
    }
+   scan_from_file(in, p);
 
-   scan_from_file(in, process_id, burst_time, arrival_time);
+   // FCFS Algorithm
+   int cct = 0;
+   int i = 0;
+   bool done = false;
+   while (!done)
+   {
+      cct += p[i].bt;
+      p[i].ct += cct;
+      p[i].tt = p[i].ct - p[i].at;
+      p[i].wt = p[i].tt - p[i].bt;
+      p[i].flag = true;
 
-   calc_complete_time(complete_time, burst_time);
-   calc_turnaround_time(turnaround_time, arrival_time, complete_time);
-   calc_waiting_time(waiting_time, burst_time, turnaround_time);
+      done = isDone(p);
+      i++;
+   }
 
-   print_to_file(out, process_id, burst_time, arrival_time, complete_time, turnaround_time, waiting_time);
-
+   print_to_file(out, p);
    // Close input - output file
    fclose(in);
-
    fclose(out);
-
    return 0;
 }
 
-void scan_from_file(FILE *in, int process_id[], int burst_time[], int arrival_time[])
+void scan_from_file(FILE *in, P p[])
 {
    int a, b, c;
 
    int i = 0;
    while (fscanf(in, "%d %d %d", &a, &b, &c) != -1)
    {
-      process_id[i] = a;
-      burst_time[i] = b;
-      arrival_time[i] = c;
+      p[i].id = a;
+      p[i].bt = b;
+      p[i].at = c;
+
+      p[i].flag = false;
+
+      p[i].ct = 0;
+      p[i].tt = 0;
+      p[i].wt = 0;
+
       i++;
    }
 }
 
-void calc_complete_time(int complete_time[], int burst_time[])
+void print_to_file(FILE *out, P p[])
 {
-   int temp = 0;
+   fprintf(out, "%s %s %s %s %s %s %s\n", "PID", "B", "A", "C", "T", "W", "D");
 
    int i;
    for (i = 0; i < NUMBER_OF_PROCESS; i++)
    {
-      temp += burst_time[i];
-      complete_time[i] += temp;
+      fprintf(out, "%d %d %d %d %d %d %s\n", p[i].id, p[i].bt, p[i].at, p[i].ct, p[i].tt, p[i].wt, p[i].flag ? "true" : "false");
    }
 }
 
-void calc_turnaround_time(int turnaround_time[], int arrival_time[], int complete_time[])
+bool isDone(P p[])
 {
-   int i;
-   for (i = 0; i < NUMBER_OF_PROCESS; i++)
+   int i = 0;
+   while (i < NUMBER_OF_PROCESS)
    {
-      turnaround_time[i] = complete_time[i] - arrival_time[i];
+      if (p[i].flag == false)
+      {
+         return false;
+      }
+      i++;
    }
-}
-
-void calc_waiting_time(int wait_time[], int burst_time[], int turnaround_time[])
-{
-   int i;
-   for (i = 0; i < NUMBER_OF_PROCESS; i++)
-   {
-      wait_time[i] = turnaround_time[i] - burst_time[i];
-   }
-}
-
-void print_to_file(FILE *out, int process_id[], int burst_time[], int arrival_time[], int complete_time[], int turnaround_time[], int wait_time[])
-{
-   fprintf(out, "%s %s %s %s %s %s\n", "PID", "B", "A", "C", "T", "W");
-
-   int i;
-   for (i = 0; i < NUMBER_OF_PROCESS; i++)
-   {
-      fprintf(out, "%d %d %d %d %d %d\n", process_id[i], burst_time[i], arrival_time[i], complete_time[i], turnaround_time[i], wait_time[i]);
-   }
+   return true;
 }
