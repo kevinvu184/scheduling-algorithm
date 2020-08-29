220aa864c2ea38969c090fe14b3c699cdd383f35
Kevin Vu <kevinvu184@gmail.com>
2020-08-29 11:37:22 +1000

    Refactor

--- FCFS.c
+++ FCFS.c
@@ -6,12 +6,17 @@
 
 typedef struct P
 {
-    int id;
+    // Burst Time
     int bt;
+    // Arrival Time
     int at;
+    // Flag indicate is done or not
     int fl;
+    // Completion time
     int ct;
+    // Turn around time
     int tt;
+    // Waiting time
     int wt;
 } P;
 
@@ -26,6 +31,7 @@ int main()
     P p[NUMBER_OF_PROCESS];
 
     FILE *in;
+    // if ((in = fopen("processes", "r")) == NULL)
     if ((in = fopen("processes", "r")) == NULL)
     {
         printf("Error - Opening processes file.");
@@ -46,17 +52,17 @@ int main()
     int done = 0;
     while (!done)
     {
-        done = isDone(p);
-        if (done)
-        {
-            break;
-        }
-
         cct += p[i].bt;
         p[i].ct += cct;
         p[i].tt = p[i].ct - p[i].at;
         p[i].wt = p[i].tt - p[i].bt;
         p[i].fl = 1;
+
+        done = isDone(p);
+        if (done)
+        {
+            break;
+        }
 
         i++;
     }
@@ -73,30 +79,27 @@ int main()
 void scan_from_file(FILE *in, P p[])
 {
     int a, b, c;
-
     int i = 0;
     while (fscanf(in, "%d %d %d", &a, &b, &c) != -1)
     {
-        p[i].id = a;
         p[i].bt = b;
         p[i].at = c;
         p[i].fl = 0;
         p[i].ct = 0;
         p[i].tt = 0;
         p[i].wt = 0;
-
         i++;
     }
 }
 
 void print_to_file(FILE *out, P p[])
 {
-    fprintf(out, "%5s%5s%5s%5s%5s%5s%5s\n", "I", "B", "A", "C", "T", "W", "D");
-
-    int i;
-    for (i = 0; i < NUMBER_OF_PROCESS; i++)
+    fprintf(out, "%5s%5s%5s%5s%5s%5s\n", "I", "B", "A", "C", "T", "W");
+    int i = 0;
+    while (i < NUMBER_OF_PROCESS)
     {
-        fprintf(out, "%5d%5d%5d%5d%5d%5d%5d\n", p[i].id, p[i].bt, p[i].at, p[i].ct, p[i].tt, p[i].wt, p[i].fl);
+        fprintf(out, "%5d%5d%5d%5d%5d%5d\n", i, p[i].bt, p[i].at, p[i].ct, p[i].tt, p[i].wt);
+        i++;
     }
 }
 
